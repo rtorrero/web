ce4418d7d33eee136ddc2f174845a7d2dc40dac2
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2022-05-18 12:51:37 +0100

    Prettify all the things

--- test/e2e/cypress/integration/hana_cluster_details.js
+++ test/e2e/cypress/integration/hana_cluster_details.js
@@ -1,56 +1,56 @@
-import { availableHanaCluster } from "../fixtures/hana-cluster-details/available_hana_cluster";
+import { availableHanaCluster } from '../fixtures/hana-cluster-details/available_hana_cluster';
 
-context("HANA database details", () => {
+context('HANA database details', () => {
   before(() => {
-    cy.loadScenario("healthy-27-node-SAP-cluster");
+    cy.loadScenario('healthy-27-node-SAP-cluster');
     cy.login();
     cy.visit(`/clusters/${availableHanaCluster.id}`);
-    cy.url().should("include", `/clusters/${availableHanaCluster.id}`);
+    cy.url().should('include', `/clusters/${availableHanaCluster.id}`);
   });
 
-  describe("HANA cluster details should be consistent with the state of the cluster", () => {
+  describe('HANA cluster details should be consistent with the state of the cluster', () => {
     it(`should have name ${availableHanaCluster.name}`, () => {
-      cy.get("div")
-        .contains("Cluster name")
+      cy.get('div')
+        .contains('Cluster name')
         .next()
         .contains(availableHanaCluster.name);
     });
 
     it(`should have sid ${availableHanaCluster.sid}`, () => {
-      cy.get("div").contains("SID").next().contains(availableHanaCluster.sid);
+      cy.get('div').contains('SID').next().contains(availableHanaCluster.sid);
     });
 
     it(`should have cluster type ${availableHanaCluster.clusterType}`, () => {
-      cy.get("div")
-        .contains("Cluster type")
+      cy.get('div')
+        .contains('Cluster type')
         .next()
         .contains(availableHanaCluster.clusterType);
     });
 
     it(`should have log replication mode ${availableHanaCluster.hanaSystemReplicationMode}`, () => {
-      cy.get("div")
-        .contains("HANA log replication mode")
+      cy.get('div')
+        .contains('HANA log replication mode')
         .next()
         .contains(availableHanaCluster.hanaSystemReplicationMode);
     });
 
     it(`should have fencing type ${availableHanaCluster.fencingType}`, () => {
-      cy.get("div")
-        .contains("Fencing type")
+      cy.get('div')
+        .contains('Fencing type')
         .next()
         .contains(availableHanaCluster.fencingType);
     });
 
     it(`should have HANA secondary sync state ${availableHanaCluster.hanaSecondarySyncState}`, () => {
-      cy.get("div")
-        .contains("HANA secondary sync state")
+      cy.get('div')
+        .contains('HANA secondary sync state')
         .next()
         .contains(availableHanaCluster.hanaSecondarySyncState);
     });
 
     it(`should have sap hana sr health state ${availableHanaCluster.sapHanaSRHealthState}`, () => {
-      cy.get("div")
-        .contains("SAPHanaSR health state")
+      cy.get('div')
+        .contains('SAPHanaSR health state')
         .next()
         .contains(availableHanaCluster.sapHanaSRHealthState);
     });
@@ -61,74 +61,74 @@ context("HANA database details", () => {
     // });
 
     it(`should have hana log operation mode ${availableHanaCluster.hanaSystemReplicationOperationMode}`, () => {
-      cy.get("div")
-        .contains("HANA log operation mode")
+      cy.get('div')
+        .contains('HANA log operation mode')
         .next()
         .contains(availableHanaCluster.hanaSystemReplicationOperationMode);
     });
   });
 
-  describe("Cluster sites should have the expected hosts", () => {
+  describe('Cluster sites should have the expected hosts', () => {
     availableHanaCluster.sites.forEach((site) => {
       it(`should have ${site.name}`, () => {
-        cy.get("h3").contains(site.name);
+        cy.get('h3').contains(site.name);
       });
 
       site.hosts.forEach((host) => {
         it(`site ${site.name} should have host ${host.hostname}`, () => {
-          cy.get("h3")
+          cy.get('h3')
             .contains(site.name)
             .siblings()
-            .find("p")
+            .find('p')
             .contains(host.hostname);
         });
 
         it(`${host.hostname} should have the expected IP addresses`, () => {
           host.ips.forEach((ip) => {
-            cy.get("h3").contains(site.name).siblings().find("td").contains(ip);
+            cy.get('h3').contains(site.name).siblings().find('td').contains(ip);
           });
         });
 
         it(`${host.hostname} should have the expected virtual IP addresses`, () => {
           host.virtualIps.forEach((ip) => {
-            cy.get("h3").contains(site.name).siblings().find("p").contains(ip);
+            cy.get('h3').contains(site.name).siblings().find('p').contains(ip);
           });
         });
 
         it(`${host.hostname} should have the expected role`, () => {
-          cy.get("h3")
+          cy.get('h3')
             .contains(site.name)
             .siblings()
-            .find("p")
+            .find('p')
             .contains(host.role);
         });
 
         it(`${host.hostname} should have the expected attributes and resources`, () => {
-          cy.get("tr > td > p")
+          cy.get('tr > td > p')
             .contains(host.hostname)
-            .parentsUntil("tbody")
-            .find("button")
-            .contains("Details")
+            .parentsUntil('tbody')
+            .find('button')
+            .contains('Details')
             .each(($el) => {
               cy.wrap($el).click();
               host.attributes.forEach(({ attribute, value }) => {
-                cy.get("p").contains(attribute).parent().next().contains(value);
+                cy.get('p').contains(attribute).parent().next().contains(value);
               });
               host.resources.forEach(
                 ({ id, role, type, status, failCount }) => {
-                  cy.get("p")
-                    .contains(new RegExp("^" + id + "$", "g"))
-                    .parentsUntil("tbody")
-                    .find("p")
+                  cy.get('p')
+                    .contains(new RegExp('^' + id + '$', 'g'))
+                    .parentsUntil('tbody')
+                    .find('p')
                     .contains(role)
-                    .parentsUntil("tbody")
-                    .find("p")
+                    .parentsUntil('tbody')
+                    .find('p')
                     .contains(status)
-                    .parentsUntil("tbody")
-                    .find("p")
+                    .parentsUntil('tbody')
+                    .find('p')
                     .contains(type)
-                    .parentsUntil("tbody")
-                    .find("p")
+                    .parentsUntil('tbody')
+                    .find('p')
                     .contains(failCount);
                 }
               );
@@ -139,15 +139,15 @@ context("HANA database details", () => {
     });
   });
 
-  describe("Cluster SBD should have the expected devices with the correct status", () => {
+  describe('Cluster SBD should have the expected devices with the correct status', () => {
     availableHanaCluster.sbd.forEach((item) => {
       it(`should have SBD device name "${item.deviceName}" and status "${item.status}"`, () => {
-        cy.get("div > div > h2")
-          .contains("SBD/Fencing")
+        cy.get('div > div > h2')
+          .contains('SBD/Fencing')
           .parent()
           .parent()
           .next()
-          .find("div")
+          .find('div')
           .contains(item.deviceName)
           .children()
           .contains(item.status);
@@ -155,57 +155,57 @@ context("HANA database details", () => {
     });
   });
 
-  describe("Settings should allow to enable checks from the checks catalog", () => {
-    it("should take me to the cluster settings when pressing the settings button", () => {
-      cy.get("button").contains("Settings").click();
+  describe('Settings should allow to enable checks from the checks catalog', () => {
+    it('should take me to the cluster settings when pressing the settings button', () => {
+      cy.get('button').contains('Settings').click();
     });
 
-    it("should include the relevant checks section", () => {
-      cy.get("div > h3").contains("Corosync");
-      cy.get("div > h3").contains("Miscellaneous");
-      cy.get("div > h3").contains("OS and package versions");
-      cy.get("div > h3").contains("Pacemaker");
-      cy.get("div > h3").contains("SBD");
+    it('should include the relevant checks section', () => {
+      cy.get('div > h3').contains('Corosync');
+      cy.get('div > h3').contains('Miscellaneous');
+      cy.get('div > h3').contains('OS and package versions');
+      cy.get('div > h3').contains('Pacemaker');
+      cy.get('div > h3').contains('SBD');
     });
 
-    it("should include the checks catalog in the checks results once enabled", () => {
-      cy.get("div > h3")
-        .contains("Corosync")
+    it('should include the checks catalog in the checks results once enabled', () => {
+      cy.get('div > h3')
+        .contains('Corosync')
         .parent()
         .siblings()
-        .find("button")
+        .find('button')
         .click();
-      cy.get("div > h3")
-        .contains("Miscellaneous")
+      cy.get('div > h3')
+        .contains('Miscellaneous')
         .parent()
         .siblings()
-        .find("button")
+        .find('button')
         .click();
-      cy.get("div > h3")
-        .contains("OS and package versions")
+      cy.get('div > h3')
+        .contains('OS and package versions')
         .parent()
         .siblings()
-        .find("button")
+        .find('button')
         .click();
-      cy.get("div > h3")
-        .contains("Pacemaker")
+      cy.get('div > h3')
+        .contains('Pacemaker')
         .parent()
         .siblings()
-        .find("button")
+        .find('button')
         .click();
-      cy.get("div > h3")
-        .contains("SBD")
+      cy.get('div > h3')
+        .contains('SBD')
         .parent()
         .siblings()
-        .find("button")
+        .find('button')
         .click();
-      cy.get("button").contains("Select Checks for Execution").click();
-      cy.get("p")
-        .contains("Well done! To start execution now, click here 👉")
+      cy.get('button').contains('Select Checks for Execution').click();
+      cy.get('p')
+        .contains('Well done! To start execution now, click here 👉')
         .siblings()
         .first()
         .click();
-      cy.get("div").find("tr").should("have.length", 72);
+      cy.get('div').find('tr').should('have.length', 72);
     });
   });
 });
