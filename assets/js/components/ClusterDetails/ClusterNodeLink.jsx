aeaea98ccc0016296d837b86804578f9968932f5
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2023-07-11 08:00:52 +0100

    wip

---
+++ assets/js/components/ClusterDetails/ClusterNodeLink.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import HostLink from '@components/HostLink';
+
+function ClusterNodeLink({ name, hostId }) {
+  if (hostId) {
+    return <HostLink hostId={hostId}>{name}</HostLink>;
+  }
+  return (
+    <span className="group flex items-center relative">
+      <EOS_WARNING_OUTLINED size="base" className="centered fill-yellow-500" />
+      <span className="ml-1 truncate max-w-[100px]">{name}</span>
+      <Tooltip
+        tooltipText="Host currently not registered."
+        width="w-52 -translate-x-1/3"
+      />
+    </span>
+  );
+}
+
+export default ClusterNodeLink;
