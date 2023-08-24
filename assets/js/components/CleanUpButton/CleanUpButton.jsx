c94293544fd3621159c84b59ec15cdef4f653306
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2023-08-24 09:27:52 +0100

    WIP

--- assets/js/components/CleanUpButton/CleanUpButton.jsx
+++ assets/js/components/CleanUpButton/CleanUpButton.jsx
@@ -1,40 +1,40 @@
 import React from 'react';
 
 import { EOS_CLEANING_SERVICES } from 'eos-icons-react';
-import classNames from 'classnames';
 
 import Button from '@components/Button';
 import Spinner from '@components/Spinner';
 
-function CleanUpButton({ cleaning, onClick, className }) {
-  const buttonClasses = classNames(
-    'inline-block mx-0.5 border-green-500 border w-fit',
-    className
-  );
-
-  return (
-    <Button
-      type="primary-white"
-      className={buttonClasses}
-      size="small"
-      disabled={cleaning}
-      onClick={() => onClick()}
-    >
-      {cleaning === true ? (
-        <Spinner className="justify-center flex" />
-      ) : (
-        <>
-          <EOS_CLEANING_SERVICES
-            size="base"
-            className="fill-jungle-green-500 inline"
-          />
-          <span className="text-jungle-green-500 text-sm font-bold pl-1.5">
-            Clean up
-          </span>
-        </>
-      )}
-    </Button>
-  );
+function CleanUpButton({ cleaning, onClick, noButtonStyle }) {
+  if (noButtonStyle) {
+    <span className="text-jungle-green-500 hover:opacity-75">
+      <Link to={`/fixme`}></Link>
+    </span>;
+  } else {
+    return (
+      <Button
+        type="primary-white"
+        className="inline-block mx-0.5 border-green-500 border w-fit"
+        size="small"
+        disabled={cleaning}
+        onClick={() => onClick()}
+      >
+        {cleaning === true ? (
+          <Spinner className="justify-center flex" />
+        ) : (
+          <>
+            <EOS_CLEANING_SERVICES
+              size="base"
+              className="fill-jungle-green-500 inline"
+            />
+            <span className="text-jungle-green-500 text-sm font-bold pl-1.5">
+              Clean up
+            </span>
+          </>
+        )}
+      </Button>
+    );
+  }
 }
 
 export default CleanUpButton;
