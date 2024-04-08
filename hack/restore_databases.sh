05a08e423fcef5050f38bf55870ad857a2a7c40a
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2024-04-08 08:22:52 +0100

    Testing

--- hack/restore_databases.sh
+++ hack/restore_databases.sh
@@ -13,11 +13,12 @@ done
 echo "PostgreSQL started."
 
 # Create databases
-createdb -h localhost -p 5432 -U postgres trento_regression_test
-createdb -h localhost -p 5432 -U postgres trento_eventstore_regression_test
+createdb -h localhost -p 5432 -U postgres trento_dev
+createdb -h localhost -p 5432 -U postgres trento_eventstore_dev
+
 
 # Restore databases from dump files
-pg_restore -h localhost -p 5432 -U postgres -d trento_regression_test -c -1 /dumps/trento_dev.dump
-pg_restore -h localhost -p 5432 -U postgres -d trento_eventstore_regression_test -c -1 /dumps/trento_eventstore_dev.dump
+pg_restore -h localhost -p 5432 -U postgres -d trento_dev -c -1 /dumps/trento_dev.dump
+pg_restore -h localhost -p 5432 -U postgres -d trento_eventstore_dev -c -1 /dumps/trento_eventstore_dev.dump
 
 echo "Database restoration complete."
