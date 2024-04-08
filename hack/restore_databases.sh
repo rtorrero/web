52994c56e327f93609a46e86d468e6e0e68dc329
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2024-04-08 16:16:11 +0100

    Use correct port

--- hack/restore_databases.sh
+++ hack/restore_databases.sh
@@ -7,18 +7,18 @@
 
 # Wait for PostgreSQL to become available
 echo "Waiting for PostgreSQL to start..."
-until pg_isready -h localhost -p 5432 -U postgres; do
+until pg_isready -h localhost -p 5433 -U postgres; do
   sleep 1
 done
 echo "PostgreSQL started."
 
 # Create databases
-createdb -h localhost -p 5432 -U postgres trento_dev
-createdb -h localhost -p 5432 -U postgres trento_eventstore_dev
+createdb -h localhost -p 5433 -U postgres trento_dev
+createdb -h localhost -p 5433 -U postgres trento_eventstore_dev
 
 
 # Restore databases from dump files
-pg_restore -h localhost -p 5432 -U postgres -d trento_dev -c -1 /dumps/trento_dev.dump
-pg_restore -h localhost -p 5432 -U postgres -d trento_eventstore_dev -c -1 /dumps/trento_eventstore_dev.dump
+pg_restore -h localhost -p 5433 -U postgres -d trento_dev -c -1 /dumps/trento_dev.dump
+pg_restore -h localhost -p 5433 -U postgres -d trento_eventstore_dev -c -1 /dumps/trento_eventstore_dev.dump
 
 echo "Database restoration complete."
