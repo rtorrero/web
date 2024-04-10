1fc4a24faec68c39f688a79beadc0de2747f3fc5
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2024-04-10 07:53:42 +0100

    Dump with create database instead of separate create

--- test/fixtures/regression/sap_system_split/trento_dev.sql
+++ test/fixtures/regression/sap_system_split/trento_dev.sql
@@ -16,6 +16,28 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: trento_dev; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE trento_dev WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE trento_dev OWNER TO postgres;
+
+\connect trento_dev
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
